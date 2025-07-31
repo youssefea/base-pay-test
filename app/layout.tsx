db05df0b11d65037d075828862d04da9001cf632
youssefea <youcefea99@gmail.com>
2025-07-31 13:23:19 +0100

    remove providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./theme.css";
 import "@coinbase/onchainkit/styles.css";
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
-import { Providers } from "./providers";
 
 export const viewport: Viewport = {
   width: "device-width",
